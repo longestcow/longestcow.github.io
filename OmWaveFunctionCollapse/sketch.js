7e6af6158193820e7fcbf82e9a0e75692474e012
longestcow <omiyer76@gmail.com>
2023-10-31 23:07:20 -0600

    ISLANDS

--- OmWaveFunctionCollapse/sketch.js
+++ OmWaveFunctionCollapse/sketch.js
@@ -2,36 +2,36 @@
 // Om Iyer
 // 30 Oct 2023
 
-p5.disableFriendlyErrors = true;
 
-const c = 2000; //canvas size
-const s = 20; // tile count per row
+const c = 1000; //canvas size
+const s = 10; // tile count per row
 let grid;
 let scl = c/s;
 
-let images = [];
+let images;
+let start,count=0;
 
 const dirs = [
-  [111,111,111,111], // blank sides
-  [121,121,111,121], // up sides
-  [121,121,121,111], // right sides
-  [111,121,121,121], // down sides
-  [121,111,121,121] // left sides
+  [[111,112,221,111],[111,112,222,211],[111,112,222,211],[111,112,222,211],[111,111,122,211],[111,111,111,111]],//row 1
+  [[122,223,331,111],[222,222,223,322],[222,222,222,222],[222,222,222,222],[221,122,222,222],[111,111,122,221]],//row 2
+  [[133,331,111,111],[322,233,311,133],[222,233,333,332],[222,233,333,332],[222,233,333,332],[221,111,133,332]],//row 3
+  [[111,111,111,111],[113,311,111,111],[333,311,111,113],[333,311,111,113],[333,311,111,113],[331,111,111,113]]//row 4
 ];
 
-let start;
 
 function preload(){
-  images[0]=loadImage("blank.jpg");
-  images[1]=loadImage("dir.jpg");
-  images[2]=images[1];
-  images[3]=images[1];
-  images[4]=images[1];
+  images=new Array(4);
+  for(let i = 1; i<=4; i++){
+    images[i-1]=new Array(6);
+    for(let j = 1; j<=6; j++){
+      images[i-1][j-1]=loadImage("images/row-"+i+"-column-"+j+".jpg");
+    }
+  }
 }
 
 
 function setup() {
-  for(let image of images) image.resize(0,scl);
+  for(let row of images) for(let image of row)image.resize(0,scl);
   grid=new Array(s);
   //fill grid
   for(let i = 0; i<s; i++){
@@ -61,8 +61,8 @@ function draw(){
     for(let j = 0; j<s; j++)
       gridCopy=gridCopy.concat(grid[i][j]);
   //now gridCopy has all tiles in one single row
-  gridCopy=gridCopy.filter((tile)=>{//remove all collapsed tiles
-    return !(tile.collapsed);
+  gridCopy=gridCopy.filter((tile)=>{
+    return (!tile.collapsed)
   });
   gridCopy.sort((a,b)=>{
     return a.options.length - b.options.length; // if a is smaller than b, it will return a negative value, thus letting the sort know which one comes first
@@ -76,12 +76,16 @@ function draw(){
   let leastEntropy = gridCopy[0].options.length;
 
   let tile=random(gridCopy.filter((tile)=>{
-    return tile.options.length === leastEntropy;
+    return (tile.options.length === leastEntropy);
   }));
   
   tile.collapse();
-
+  print(gridCopy);
 }
+
+// function mousePressed(){
+//   loop();
+// }
 
 
 class Tile{
@@ -91,37 +95,35 @@ class Tile{
     this.x = x;
     this.y = y;
     this.collapsed=false;
-    this.options = [0,1,2,3,4];
-    this.rot;
+    let arr = [], k = 0;
+    for(let i = 0; i<4; i++)
+      for(let j = 0; j<6; j++, k++)
+        arr[k]=[i,j];
+    
+    this.options = arr; //starts off with all possibilities
   }
 
 
   draw(){
-    if(this.collapsed){
-      push();
-      translate(this.x+scl/2,this.y+scl/2);
-      rotate((HALF_PI)*this.rot);
-      image(images[this.options[0]], -scl/2, -scl/2);
-      pop();
-    }
+    if(this.collapsed)
+      image(images[this.options[0][0]][this.options[0][1]], this.x, this.y);
     else
-      image(images[0], this.x, this.y); 
+      image(images[0][5], this.x, this.y); 
   }
 
   collapse(){//update neighbours
     let pick = random(this.options);
-    if(pick===undefined)restart();
+    if(pick===undefined)restart(this);
     this.options=[pick];
-    this.sockets=dirs[pick];
+    this.sockets=dirs[pick[0]][pick[1]];
     this.collapsed = true;
-    this.rot=(pick!==0)?pick-1:0;
 
-    //update up's down, filter out any downs that are not equal to this guy's up
+    // update up's down
     let x=this.x/scl,y=this.y/scl,x1,y1;
     if(y !== 0){
       y1=this.y/scl-1;
       grid[x][y1].options = grid[x][y1].options.filter((n)=>{
-        return(dirs[n][2] === dirs[pick][0]); //filter out the ones whose down are not equal to this guy's up
+        return(reverseInt(dirs[n[0]][n[1]][2]) === dirs[pick[0]][pick[1]][0]); //filter out the ones whose down are not equal to this guy's up
       });
     }
 
@@ -129,21 +131,21 @@ class Tile{
     if(x !== s-1){
       x1=this.x/scl+1;
       grid[x1][y].options = grid[x1][y].options.filter((n)=>{
-        return(dirs[n][3] === dirs[pick][1]); //filter out the ones whose left are not equal to this guy's right
+        return(reverseInt(dirs[n[0]][n[1]][3]) === dirs[pick[0]][pick[1]][1]); //filter out the ones whose left are not equal to this guy's right
       });
     }
 
     if(y !== s-1){ //update down's up
       y1=this.y/scl+1;
       grid[x][y1].options = grid[x][y1].options.filter((n)=>{
-        return(dirs[n][0] === dirs[pick][2]); //filter out the ones whose up are not equal to this guy's down
+        return(reverseInt(dirs[n[0]][n[1]][0]) === dirs[pick[0]][pick[1]][2]); //filter out the ones whose up are not equal to this guy's down
       });
     }
 
     if(x !== 0){//update left's right
       x1=this.x/scl-1;
       grid[x1][y].options= grid[x1][y].options.filter((n)=>{
-        return(dirs[n][1] === dirs[pick][3]); //filter out the ones whose left are not equal to this guy's right
+        return(reverseInt(dirs[n[0]][n[1]][1]) === dirs[pick[0]][pick[1]][3]); //filter out the ones whose left are not equal to this guy's right
       });
     }
 
@@ -153,8 +155,21 @@ class Tile{
 
 }
 
-function restart(){
+function restart(tile){
   noLoop();
-  print("couldnt reach end");
+  print("couldnt reach end "+tile.x+","+tile.y);
+  print(grid);
   setup();
+}
+
+function reverseInt(n){//faster than the built in function
+  var d, a = 0
+
+  while(n){
+      d = n % 10;
+      a = (a*10) + d;
+      n = n/10|0;
+  }  
+
+  return a;
 }
